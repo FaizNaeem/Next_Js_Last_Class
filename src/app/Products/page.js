32c573884bdb2e7541c771db51e651d69adbf9cd
FaizNaeem <faiznaeem192022@gmail.com>
2023-10-03 18:59:02 +0500

    first commit

---
+++ src/app/Products/page.js
@@ -0,0 +1,28 @@
+import getData from '@/lib/product'
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default async function Products() {
+const product = await getData()
+console.log(product);
+  return (
+    <main className="">
+ <h1 className='text-3xl text-center'>Products</h1>
+ <div className='flex flex-row justify-between items-center flex-wrap w-full'>
+    {
+        product.map((e,i)=>{
+  return(
+    <>
+<div className='w-70 flex flex-col items-center '>
+<Image src={e.image} width={200}/>
+</div>
+    </>
+  )
+
+        })
+    }
+
+ </div>
+    </main>
+  )
+}
