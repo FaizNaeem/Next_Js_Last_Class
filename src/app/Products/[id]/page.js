81a1691e2dd7a40bace2858cfbbfd8993ce8d26e
FaizNaeem <faiznaeem192022@gmail.com>
2023-10-04 03:06:23 +0500

    files

--- src/app/Products/[id]/page.js
+++ src/app/Products/[id]/page.js
@@ -1,7 +1,28 @@
+import details from '@/lib/detalis'
 import React from 'react'
-
-export default function Id({params}) {
+import './details.css'
+export default async function Id({params}) {
+    const detail = await details(params.id)
+    console.log(detail);
   return (
-    <div>Id : {params.id}</div>
+    <>
+        
+    <div className="hl">
+    <div className="card p-4" style={{width: "20rem;" }}>
+      
+  <img src={`${detail.image}`}  style={{width:"10rem"}} className="card-img-top p-3" alt="..."/>
+       
+  <div className="card-body">
+    <h5 class="card-title">Title: {detail.title}</h5>
+    <p class="card-text"> Price : {detail.price}</p>
+    <h5 class="card-title">Age: {}</h5>
+    <a href="#" className="bg-green-500 hn"  s>Hello everyOne</a>
+  </div>
+</div>
+ 
+    </div>
+      
+  {/* <Users/> */}
+    </>
   )
 }
