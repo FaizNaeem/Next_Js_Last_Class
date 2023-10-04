712b709d4b7cb2f06cb37a4ea695d9f0a524a107
FaizNaeem <faiznaeem192022@gmail.com>
2023-10-04 22:03:37 +0500

    final

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -4,19 +4,19 @@ import React, { useContext } from 'react'
 export default function Navbar() {
     const check = useContext(Authcontext)
     // console.log(check);
-    const CheckUser =()=>{
-if (check== false){
-setAuth(true)
-}
-else{
-    setAuth(false)
-}
-    }
+//     const CheckUser =()=>{
+// if (check== false){
+// setAuth("")
+// }
+// else{
+//     setAuth(false)
+// }
+//     }
   return (
     <div className='fixed-top'>
         <nav class="navbar navbar-light bg-light justify-content-between">
   <a class="navbar-brand"> <i>Faiz</i></a>
-    <button class="btn btn-danger my-2 my-sm-0" type="submit" onClick={CheckUser}>{check ? }</button>
+    <button class="btn btn-danger my-2 my-sm-0" type="submit" >Login</button>
 </nav>
     </div>
   )
